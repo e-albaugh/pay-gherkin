e9d17f488a5de73788912e062a178266fc517338
e-albaugh <51415624+e-albaugh@users.noreply.github.com>
2020-05-11 14:27:56 -0500

    add import statement

--- src/test/java/net/mf/MyStepdefs.java
+++ src/test/java/net/mf/MyStepdefs.java
@@ -8,6 +8,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
+import static java.lang.System.*;
 
 import static junit.framework.TestCase.assertTrue;
 
